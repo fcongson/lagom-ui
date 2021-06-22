ca72aac36ac923b4f8f5df538a49c10fb7f647c5
Frank Congson <fcongson@gmail.com>
2021-06-23 09:01:22 +1200

    Clean up addons and dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,13 @@
 module.exports = {
   stories: ["../src/**/*.stories.tsx"],
   addons: [
+    "storybook-addon-themes",
     {
       name: "@storybook/addon-essentials",
       options: {
         controls: false,
       },
     },
-    "storybook-addon-themes",
+    "@storybook/addon-a11y",
   ],
 };
