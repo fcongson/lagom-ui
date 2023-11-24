05087611ffdc6c8f9fc2afbe39ae2ea6a3b8d8a7
Frank Congson <fcongson@gmail.com>
2023-11-24 15:46:21 +1300

    Add classnames

--- src/components/PageHeader/PageHeader.tsx
+++ src/components/PageHeader/PageHeader.tsx
@@ -1,38 +1,35 @@
+import clsx from "clsx";
 import styled from "styled-components";
 import { useMergedTheme } from "../../hooks/useMergedTheme";
 
 const StyledHeader = styled.h1`
-  color: var(--lagom-semantic-color-fg-default);
-  font: var(--lagom-component-page-header-typography);
-  text-align: center;
-  margin: 0 auto;
-  margin-bottom: var(--lagom-core-spacing-xxl);
+  &.lagom-page-header {
+    color: var(--lagom-semantic-color-fg-default);
+    font: var(--lagom-component-page-header-typography);
+    text-align: center;
+    margin: 0 auto;
+    margin-bottom: var(--lagom-core-spacing-xxl);
 
-  ${(props) => props.theme.mediaQueries.large} {
-    font-size: var(--header-scale-2);
-  }
-  ${(props) => props.theme.mediaQueries.small} {
-    font-size: var(--header-scale-3);
-    margin-bottom: var(--lagom-core-spacing-xl);
+    ${(props) => props.theme.mediaQueries.large} {
+      font-size: var(--header-scale-2);
+    }
+    ${(props) => props.theme.mediaQueries.small} {
+      font-size: var(--header-scale-3);
+      margin-bottom: var(--lagom-core-spacing-xl);
+    }
   }
 `;
 
-// StyledHeader.defaultProps = {
-//   fontFamily: "sansSerif",
-//   fontWeight: "header",
-//   fontSize: [6, null, 7, null, 8],
-//   lineHeight: [6, null, 7, null, 8],
-//   textAlign: "center",
-//   margin: "0 auto",
-//   marginBottom: [4, null, null, null, 5],
-// };
-
 export const PageHeader: React.FunctionComponent<
   React.ComponentPropsWithoutRef<"h1">
-> = ({ children, ...restProps }) => {
+> = ({ children, className, ...restProps }) => {
   const mergedTheme = useMergedTheme();
   return (
-    <StyledHeader theme={mergedTheme} {...restProps}>
+    <StyledHeader
+      theme={mergedTheme}
+      className={clsx("lagom-page-header", className)}
+      {...restProps}
+    >
       {children}
     </StyledHeader>
   );
