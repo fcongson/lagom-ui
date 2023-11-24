219cf0e88ab9350a810f00259c64163847daf7e8
Frank Congson <fcongson@gmail.com>
2023-11-24 16:01:40 +1300

    Add classnames

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,53 +1,68 @@
+import clsx from "clsx";
 import styled from "styled-components";
 import { useMergedTheme } from "../../hooks/useMergedTheme";
 
 const HeroContainer = styled.div`
-  display: grid;
-  overflow: hidden;
-  margin: 0 auto 8rem auto;
+  &.lagom-hero {
+    width: 100%;
+    height: var(--hero-height, 100vh);
+    display: grid;
+    margin: 0 auto 8rem auto;
 
-  ${(props) => props.theme.mediaQueries.small} {
-    margin: 0 auto 4rem auto;
+    ${(props) => props.theme.mediaQueries.small} {
+      margin: 0 auto 4rem auto;
+    }
   }
 `;
 
 const HeroImage = styled.div`
-  grid-area: 1 / 1;
-  width: 100%;
-  height: 100vh;
+  &.lagom-hero__image {
+    width: 100%;
+    height: var(--hero-height, 100vh);
+    grid-area: 1/1;
+  }
 `;
 
 const HeroImageOverlay = styled.div`
-  grid-area: 1 / 1;
-  background-color: var(--lagom-semantic-color-overlay-background);
-  opacity: var(--lagom-semantic-opacity-overlay);
+  &.lagom-hero__image-overlay {
+    grid-area: 1 / 1;
+    background-color: var(--lagom-semantic-color-overlay-background);
+    opacity: var(--lagom-semantic-opacity-overlay);
+  }
 `;
 
 const HeroContent = styled.div`
-  grid-area: 1 / 1;
-  justify-self: center;
-  align-self: center;
-  z-index: 1;
-  overflow: hidden;
-  color: var(--lagom-semantic-color-fg-default);
+  &.lagom-hero__content {
+    grid-area: 1/1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    z-index: 1;
+  }
 `;
 
 export const Hero: React.FunctionComponent<{
   image: React.ReactElement;
   children?: React.ReactNode;
-}> = ({ image, children, ...restProps }) => {
+  className?: string;
+}> = ({ image, children, className, ...restProps }) => {
   const mergedTheme = useMergedTheme();
   return (
-    <HeroContainer theme={mergedTheme} {...restProps}>
+    <HeroContainer
+      theme={mergedTheme}
+      className={clsx("lagom-hero", className)}
+      {...restProps}
+    >
       {!!image ? (
         <>
-          <HeroImage>{image}</HeroImage>
+          <HeroImage className="lagom-hero__image">{image}</HeroImage>
         </>
       ) : null}
       {!!children ? (
         <>
-          <HeroImageOverlay />
-          <HeroContent>{children}</HeroContent>
+          <HeroImageOverlay className="lagom-hero__image-overlay" />
+          <HeroContent className="lagom-hero__content">{children}</HeroContent>
         </>
       ) : null}
     </HeroContainer>
