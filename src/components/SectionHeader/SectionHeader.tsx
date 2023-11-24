d20f93b168aa8a25d0c645c1cbd5bb7001cb7f68
Frank Congson <fcongson@gmail.com>
2023-11-24 15:46:21 +1300

    Add classnames

--- src/components/SectionHeader/SectionHeader.tsx
+++ src/components/SectionHeader/SectionHeader.tsx
@@ -1,38 +1,35 @@
+import clsx from "clsx";
 import styled from "styled-components";
 import { useMergedTheme } from "../../hooks/useMergedTheme";
 
 const StyledHeader = styled.h2`
-  color: var(--lagom-semantic-color-fg-default);
-  font: var(--lagom-component-section-header-typography);
-  text-align: center;
-  margin: 0 auto;
-  margin-bottom: var(--lagom-core-spacing-xxl);
+  &.lagom-section-header {
+    color: var(--lagom-semantic-color-fg-default);
+    font: var(--lagom-component-section-header-typography);
+    text-align: center;
+    margin: 0 auto;
+    margin-bottom: var(--lagom-core-spacing-xxl);
 
-  ${(props) => props.theme.mediaQueries.large} {
-    font-size: var(--header-scale-3);
-  }
-  ${(props) => props.theme.mediaQueries.small} {
-    font-size: var(--header-scale-4);
-    margin-bottom: var(--lagom-core-spacing-xl);
+    ${(props) => props.theme.mediaQueries.large} {
+      font-size: var(--header-scale-3);
+    }
+    ${(props) => props.theme.mediaQueries.small} {
+      font-size: var(--header-scale-4);
+      margin-bottom: var(--lagom-core-spacing-xl);
+    }
   }
 `;
 
-// StyledHeader.defaultProps = {
-//   fontFamily: "sansSerif",
-//   fontWeight: "header",
-//   fontSize: [5, null, 6, null, 7],
-//   lineHeight: [5, null, 6, null, 7],
-//   textAlign: "center",
-//   margin: "0 auto",
-//   marginBottom: [4, null, null, null, 5],
-// };
-
 export const SectionHeader: React.FunctionComponent<
   React.ComponentPropsWithoutRef<"h2">
-> = ({ children, ...restProps }) => {
+> = ({ children, className, ...restProps }) => {
   const mergedTheme = useMergedTheme();
   return (
-    <StyledHeader theme={mergedTheme} {...restProps}>
+    <StyledHeader
+      theme={mergedTheme}
+      className={clsx("lagom-section-header", className)}
+      {...restProps}
+    >
       {children}
     </StyledHeader>
   );
