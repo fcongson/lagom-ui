feff575c3db394e0236f939d4de91a450569cbd7
Frank Congson <fcongson@gmail.com>
2023-11-25 22:46:29 +1300

    Update image caption

--- src/components/ImageCaption/ImageCaption.tsx
+++ src/components/ImageCaption/ImageCaption.tsx
@@ -1,39 +1,24 @@
+import clsx from "clsx";
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { Container } from "../../layouts/Container";
 import { Section } from "../../layouts/Section";
 
-const Image = styled.div`
-  .block-img {
+const imageCaptionCss = css`
+  &.lagom-image-caption.lagom-section {
+    overflow: hidden;
+  }
+
+  .lagom-image-caption__block-img {
     margin-bottom: 2rem;
-
-    img {
-      width: 100%;
-      height: auto;
-    }
   }
 
-  .emphasized {
-    ${({ theme }) => theme.mediaQueries.emphasized} {
-      width: 130%;
-      margin: 0 -15% 2rem -15%;
-    }
+  .lagom-image-caption__block-img img {
+    width: 100%;
+    height: auto;
   }
 
-  .full-width {
-    max-height: 100vh;
-    margin-bottom: 8rem;
-
-    div {
-      max-height: 100vh;
-    }
-
-    ${({ theme }) => theme.mediaQueries.small} {
-      margin-bottom: 4rem;
-    }
-  }
-
-  .image-label {
+  .lagom-image-caption__image-label {
     display: block;
     text-align: center;
     font-style: italic;
@@ -43,63 +28,104 @@ const Image = styled.div`
 `;
 
 type ImageProps = {
+  className?: string;
   image: React.ReactNode;
   caption?: string;
 };
 
+const DefaultImageSection = styled(Section)`
+  ${imageCaptionCss}
+`;
+
 const DefaultImage: React.FunctionComponent<ImageProps> = ({
+  className,
   image,
   caption,
 }) => {
   return (
-    <Image>
-      <Section>
-        <Container>
-          <figure className="block-img">
-            {image}
-            {caption && caption !== "" ? (
-              <figcaption className="image-label">{caption}</figcaption>
-            ) : null}
-          </figure>
-        </Container>
-      </Section>
-    </Image>
+    <DefaultImageSection className={clsx("lagom-image-caption", className)}>
+      <Container>
+        <figure className="lagom-image-caption__block-img">
+          {image}
+          {caption && caption !== "" ? (
+            <figcaption className="lagom-image-caption__image-label">
+              {caption}
+            </figcaption>
+          ) : null}
+        </figure>
+      </Container>
+    </DefaultImageSection>
   );
 };
+
+const EmphasizedImageSection = styled(Section)`
+  ${imageCaptionCss}
+
+  .lagom-image-caption--emphasized {
+    ${({ theme }) => theme.mediaQueries.emphasized} {
+      width: 130%;
+      margin: 0 -15% 2rem -15%;
+    }
+  }
+`;
 
 const EmphasizedImage: React.FunctionComponent<ImageProps> = ({
+  className,
   image,
   caption,
 }) => {
   return (
-    <Image>
-      <Section>
-        <Container>
-          <figure className="block-img emphasized">
-            {image}
-            {caption && caption !== "" ? (
-              <figcaption className="image-label">{caption}</figcaption>
-            ) : null}
-          </figure>
-        </Container>
-      </Section>
-    </Image>
+    <EmphasizedImageSection className={clsx("lagom-image-caption", className)}>
+      <Container>
+        <figure className="lagom-image-caption__block-img lagom-image-caption--emphasized">
+          {image}
+          {caption && caption !== "" ? (
+            <figcaption className="lagom-image-caption__image-label">
+              {caption}
+            </figcaption>
+          ) : null}
+        </figure>
+      </Container>
+    </EmphasizedImageSection>
   );
 };
 
+const FullWidthImageFigure = styled.figure`
+  ${imageCaptionCss}
+
+  &.lagom-image-caption--full-width {
+    max-height: 100vh;
+    margin-bottom: 8rem;
+
+    ${({ theme }) => theme.mediaQueries.small} {
+      margin-bottom: 4rem;
+    }
+  }
+
+  &.lagom-image-caption--full-width div {
+    max-height: 100vh;
+  }
+`;
+
 const FullWidthImage: React.FunctionComponent<ImageProps> = ({
+  className,
   image,
   caption,
 }) => {
   return (
-    <Image>
-      <figure className="block-img full-width">
-        <div>{image}</div>
-        {caption && caption !== "" ? (
-          <figcaption className="image-label">{caption}</figcaption>
-        ) : null}
-      </figure>
-    </Image>
+    <FullWidthImageFigure
+      className={clsx(
+        "lagom-image-caption lagom-image-caption__block-img lagom-image-caption--full-width",
+        className,
+      )}
+    >
+      <div>{image}</div>
+      {caption && caption !== "" ? (
+        <figcaption className="lagom-image-caption__image-label">
+          {caption}
+        </figcaption>
+      ) : null}
+    </FullWidthImageFigure>
   );
 };
 
