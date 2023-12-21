3d10ee8bd6a80b52217bb42087278ebb111d9b06
Frank Congson <fcongson@gmail.com>
2023-12-21 15:34:39 +1300

    Add semantic colors

--- src/stories/design-tokens/Color.stories.tsx
+++ src/stories/design-tokens/Color.stories.tsx
@@ -1,4 +1,7 @@
 import * as core from "@fcongson/lagom-tokens/js/variables/core";
+import * as semantic from "@fcongson/lagom-tokens/js/variables/semantic";
+import * as semanticDark from "@fcongson/lagom-tokens/js/variables/semantic.dark";
+import * as semanticLight from "@fcongson/lagom-tokens/js/variables/semantic.light";
 import { Meta, StoryObj } from "@storybook/react";
 import React from "react";
 import { Container, PageHeader, Section, SectionHeader } from "../..";
@@ -11,6 +14,7 @@ const meta: Meta = {
 export default meta;
 
 const TOKEN_KEY = "lagomCoreColors";
+const TOKEN_KEY_SEMANTIC = "lagomSemanticColor";
 
 interface ColorCardProps {
   color: string;
@@ -58,21 +62,38 @@ const ColorCard: React.FunctionComponent<ColorCardProps> = ({
 );
 
 interface ColorGroupProps {
-  color: string;
+  color?: string;
+  title?: string;
+  tokenSet: Object;
+  tokenKey?: string;
 }
 
-const ColorGroup: React.FunctionComponent<ColorGroupProps> = ({ color }) => (
-  <Section>
-    <Container>
-      <SectionHeader>{color}</SectionHeader>
-      {Object.keys(core).map((key) => {
-        return key.startsWith(`${TOKEN_KEY}${color}`) ? (
-          <ColorCard name={key} color={core[key]} />
-        ) : undefined;
-      })}
-    </Container>
-  </Section>
-);
+const ColorGroup: React.FunctionComponent<ColorGroupProps> = ({
+  color,
+  title,
+  tokenSet,
+  tokenKey = TOKEN_KEY,
+}) => {
+  const colors = Object.keys(tokenSet).filter((key) =>
+    key.startsWith(`${tokenKey}${color ?? ""}`),
+  );
+  console.log({
+    tokenSet,
+    tokenKey,
+    startsWith: `${tokenKey}${color}`,
+    colors,
+  });
+  return (
+    <Section>
+      <Container>
+        <SectionHeader>{title || color}</SectionHeader>
+        {colors.map((color) => (
+          <ColorCard name={color} color={tokenSet[color]} />
+        ))}
+      </Container>
+    </Section>
+  );
+};
 
 export const Color: StoryObj = {
   render: () => (
@@ -96,12 +117,27 @@ export const Color: StoryObj = {
           />
         </Container>
       </Section>
-      <ColorGroup color="Grey" />
-      <ColorGroup color="Olive" />
-      <ColorGroup color="Violet" />
-      <ColorGroup color="Navy" />
-      <ColorGroup color="Red" />
-      <ColorGroup color="Honey" />
+      <ColorGroup color="Grey" tokenSet={core} />
+      <ColorGroup color="Olive" tokenSet={core} />
+      <ColorGroup color="Violet" tokenSet={core} />
+      <ColorGroup color="Navy" tokenSet={core} />
+      <ColorGroup color="Red" tokenSet={core} />
+      <ColorGroup color="Honey" tokenSet={core} />
+      <ColorGroup
+        title="Semantic"
+        tokenSet={semantic}
+        tokenKey={TOKEN_KEY_SEMANTIC}
+      />
+      <ColorGroup
+        title="Semantic Light"
+        tokenSet={semanticLight}
+        tokenKey={TOKEN_KEY_SEMANTIC}
+      />
+      <ColorGroup
+        title="Semantic Dark"
+        tokenSet={semanticDark}
+        tokenKey={TOKEN_KEY_SEMANTIC}
+      />
     </>
   ),
 };
